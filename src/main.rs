049718b63d4e3e16080cffda0294620ea3d80514
Jack Phoebus <jmp1996.jp@gmail.com>
2023-09-07 15:43:23 -0400

    create device

--- src/main.rs
+++ src/main.rs
@@ -75,6 +75,7 @@ struct App {
 	entry: Entry,
 	instance: Instance,
 	data: AppData,
+	device: Device,
 }
 
 impl App {
@@ -88,7 +89,9 @@ impl App {
 
 		pick_physical_device(&instance, &mut data)?;
 
-		Ok(Self {entry,instance,data})
+		let device=create_logical_device(&entry,&instance,&mut data)?;
+
+		Ok(Self {entry,instance,data,device})
 	}
 
 	/// Renders a frame for our Vulkan app.
@@ -98,6 +101,8 @@ impl App {
 
 	/// Destroys our Vulkan app.
 	unsafe fn destroy(&mut self) {
+		self.device.destroy_device(None);
+
 		if VALIDATION_ENABLED {
 			self.instance.destroy_debug_utils_messenger_ext(self.data.messenger, None);
 		}
@@ -106,11 +111,47 @@ impl App {
 	}
 }
 
+unsafe fn create_logical_device(entry: &Entry, instance: &Instance, data: &mut AppData) -> Result<Device> {
+	let indices=QueueFamilyIndices::get(instance,data,data.physical_device)?;
+
+	let queue_priorities=&[1.0];
+	let queue_info=vk::DeviceQueueCreateInfo::builder()
+		.queue_family_index(indices.graphics)
+		.queue_priorities(queue_priorities);
+
+	let layers = if VALIDATION_ENABLED {
+		vec![VALIDATION_LAYER.as_ptr()]
+	} else {
+		vec![]
+	};
+
+	let mut extensions=vec![];
+	if cfg!(target_os="macos") && entry.version()? >= PORTABILITY_MACOS_VERSION {
+		extensions.push(vk::KHR_PORTABILITY_SUBSET_EXTENSION.name.as_ptr());
+	}
+
+	let features=vk::PhysicalDeviceFeatures::builder();
+
+	let queue_infos=&[queue_info];
+	let info=vk::DeviceCreateInfo::builder()
+		.queue_create_infos(queue_infos)
+		.enabled_layer_names(&layers)
+		.enabled_extension_names(&extensions)
+		.enabled_features(&features);
+
+	let device=instance.create_device(data.physical_device,&info,None)?;
+
+	data.graphics_queue=device.get_device_queue(indices.graphics,0);
+
+	Ok(device)
+}
+
 /// The Vulkan handles and associated properties used by our Vulkan app.
 #[derive(Clone, Debug, Default)]
 struct AppData {
 	messenger: vk::DebugUtilsMessengerEXT,
 	physical_device: vk::PhysicalDevice,
+	graphics_queue: vk::Queue,
 }
 
 unsafe fn create_instance(window: &Window, entry: &Entry, data: &mut AppData)->Result<Instance>{
