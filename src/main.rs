8dc8627f4d6757569d4fcf441945e6f4efce5f7a
Jack Phoebus <jmp1996.jp@gmail.com>
2023-09-07 01:19:27 -0400

    add portability extensions

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,9 @@ use winit::event::{Event, WindowEvent};
 use winit::event_loop::{ControlFlow, EventLoop};
 use winit::window::{Window, WindowBuilder};
 
+/// The Vulkan SDK version that started requiring the portability subset extension for macOS.
+const PORTABILITY_MACOS_VERSION: Version = Version::new(1, 3, 216);
+
 fn main() -> Result<()> {
 	pretty_env_logger::init();
 
@@ -88,14 +91,27 @@ unsafe fn create_instance(window: &Window, entry: &Entry)->Result<Instance>{
 		.engine_version(vk::make_version(1,0,0))
 		.api_version(vk::make_version(1,0,0));
 
-	let extensions=vk_window::get_required_instance_extensions(window)
+	let mut extensions=vk_window::get_required_instance_extensions(window)
 		.iter()
 		.map(|e| e.as_ptr())
 		.collect::<Vec<_>>();
 
+	let flags=if
+		cfg!(target_os="macos") &&
+		entry.version()? >= PORTABILITY_MACOS_VERSION
+	{
+		info!("Enabling Extensions for macOS portability.");
+		extensions.push(vk::KHR_GET_PHYSICAL_DEVICE_PROPERTIES2_EXTENSION.name.as_ptr());
+		extensions.push(vk::KHR_PORTABILITY_ENUMERATION_EXTENSION.name.as_ptr());
+		vk::InstanceCreateFlags::ENUMERATE_PORTABILITY_KHR
+	}else{
+		vk::InstanceCreateFlags::empty()
+	};
+
 	let info=vk::InstanceCreateInfo::builder()
 		.application_info(&application_info)
-		.enabled_extension_names(&extensions);
+		.enabled_extension_names(&extensions)
+		.flags(flags);
 
 	Ok(entry.create_instance(&info,None)?)
 }
