a8a2e471ce582a897d39505c1a33bfc4bf2be66b
Jack Phoebus <jmp1996.jp@gmail.com>
2023-09-09 21:05:30 -0400

    finish pipeline

--- src/main.rs
+++ src/main.rs
@@ -97,6 +97,8 @@ impl App {
 		let device = create_logical_device(&entry, &instance, &mut data)?;
 		create_swapchain(window, &instance, &device, &mut data)?;
 		create_swapchain_image_views(&device, &mut data)?;
+
+		create_render_pass(&instance, &device, &mut data)?;
 		create_pipeline(&device, &mut data)?;
 
 		Ok(Self { entry, instance, data, device })
@@ -109,7 +111,10 @@ impl App {
 
 	/// Destroys our Vulkan app.
 	unsafe fn destroy(&mut self) {
+		self.device.destroy_pipeline(self.data.pipeline, None);
+
 		self.device.destroy_pipeline_layout(self.data.pipeline_layout, None);
+		self.device.destroy_render_pass(self.data.render_pass, None);
 
 		self.data.swapchain_image_views
 			.iter()
@@ -125,6 +130,38 @@ impl App {
 		self.instance.destroy_surface_khr(self.data.surface, None);
 		self.instance.destroy_instance(None);
 	}
+}
+
+unsafe fn create_render_pass(instance: &Instance, device: &Device, data: &mut AppData) -> Result<()> {
+	let color_attachment = vk::AttachmentDescription::builder()
+		.format(data.swapchain_format)
+		.samples(vk::SampleCountFlags::_1)
+		.load_op(vk::AttachmentLoadOp::CLEAR)
+		.store_op(vk::AttachmentStoreOp::STORE)
+		.stencil_load_op(vk::AttachmentLoadOp::DONT_CARE)
+		.stencil_store_op(vk::AttachmentStoreOp::DONT_CARE)
+		.initial_layout(vk::ImageLayout::UNDEFINED)
+		.final_layout(vk::ImageLayout::PRESENT_SRC_KHR);
+
+	let color_attachment_ref = vk::AttachmentReference::builder()
+		.attachment(0)
+		.layout(vk::ImageLayout::COLOR_ATTACHMENT_OPTIMAL);
+
+	let color_attachments = &[color_attachment_ref];
+	let subpass = vk::SubpassDescription::builder()
+		.pipeline_bind_point(vk::PipelineBindPoint::GRAPHICS)
+		.color_attachments(color_attachments);
+
+	let attachments = &[color_attachment];
+	let subpasses = &[subpass];
+	let info = vk::RenderPassCreateInfo::builder()
+		.attachments(attachments)
+		.subpasses(subpasses);
+
+	data.render_pass = device.create_render_pass(&info, None)?;
+
+
+	Ok(())
 }
 
 #[cfg(not(windows))]
@@ -210,6 +247,23 @@ unsafe fn create_pipeline(device: &Device, data: &mut AppData) -> Result<()> {
 
 	data.pipeline_layout = device.create_pipeline_layout(&layout_info, None)?;
 
+	let stages = &[vert_stage, frag_stage];
+	let info = vk::GraphicsPipelineCreateInfo::builder()
+		.stages(stages)
+		.vertex_input_state(&vertex_input_state)
+		.input_assembly_state(&input_assembly_state)
+		.viewport_state(&viewport_state)
+		.rasterization_state(&rasterization_state)
+		.multisample_state(&multisample_state)
+		.color_blend_state(&color_blend_state)
+		.layout(data.pipeline_layout)
+		.render_pass(data.render_pass)
+		.subpass(0);
+
+	data.pipeline = device.create_graphics_pipelines(
+		vk::PipelineCache::null(),
+		&[info],
+		None)?.0[0];
 
 	device.destroy_shader_module(vert_shader_module, None);
 	device.destroy_shader_module(frag_shader_module, None);
@@ -375,7 +429,9 @@ struct AppData {
 	swapchain_images: Vec<vk::Image>,
 	swapchain_image_views: Vec<vk::ImageView>,
 
+	render_pass: vk::RenderPass,
 	pipeline_layout: vk::PipelineLayout,
+	pipeline: vk::Pipeline,
 }
 
 unsafe fn create_instance(window: &Window, entry: &Entry, data: &mut AppData) -> Result<Instance> {
